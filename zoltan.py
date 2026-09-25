36eaeb15bdc31ff4bceccdc56815106934a37ccc
Jozef2004 <jozef.daniel.zoltan@gmail.com>
2026-09-25 14:40:16 +0200

    Update zoltan.py

--- zoltan.py
+++ zoltan.py
@@ -6,18 +6,6 @@ from scipy.fft import dctn, idctn
 #X = dctn(img, type=2, norm="ortho")      # orthonormal 2D DCT-II
 #img_rec = idctn(X, type=2, norm="ortho") # inverse
 
-img = tifffile.imread("bilder/rice.tif")   # np.ndarray, shape (H, W)
-
-x_length = img.shape[0]
-y_length = img.shape[1]
-
-block_size = 8
-x_blocks = int(x_length / block_size)
-y_blocks = int(y_length / block_size)
-
-blocks = img.reshape(x_blocks, block_size, y_blocks, block_size).swapaxes(1, 2)
-first_block = blocks[0, 0]
-
 def compress_block(img):
     X = dctn(img, type=2, norm="ortho")
 
@@ -34,25 +22,41 @@ def compress_block(img):
 
     return img_approx
 
-approx_blocks = np.zeros(blocks.shape, dtype=np.float64)
-for i in range(x_blocks):
-    for j in range(y_blocks):
-        approx_blocks[i, j] = compress_block(blocks[i, j])
+def compress_image(file_path, plotting = False):
+    img = tifffile.imread(file_path)   # np.ndarray, shape (H, W)
 
-img_approx = approx_blocks.swapaxes(1, 2).reshape(x_length, y_length)
+    x_length = img.shape[0]
+    y_length = img.shape[1]
 
-img_approx_clipped = np.clip(img_approx, 0, 255)
+    block_size = 8
+    if (x_length % block_size) != 0 or (y_length % block_size) != 0:
+        raise Exception("Image size not divisable by ", block_size)
 
-fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-ax[0].imshow(img, cmap="gray", vmin=0, vmax=255)
-ax[0].set_title("Original")
-ax[1].imshow(img_approx_clipped, cmap="gray", vmin=0, vmax=255)
-ax[1].set_title("Top 8 of 64 coeffs per block")
-for a in ax:
-    a.axis("off")
-plt.tight_layout()
-plt.show()
+    x_blocks = int(x_length / block_size)
+    y_blocks = int(y_length / block_size)
 
+    blocks = img.reshape(x_blocks, block_size, y_blocks, block_size).swapaxes(1, 2)
 
+    approx_blocks = np.zeros(blocks.shape, dtype=np.float64)
+    for i in range(x_blocks):
+        for j in range(y_blocks):
+            approx_blocks[i, j] = compress_block(blocks[i, j])
+
+    img_approx = approx_blocks.swapaxes(1, 2).reshape(x_length, y_length)
+
+    img_approx_clipped = np.clip(img_approx, 0, 255)
+
+    if plotting == True:
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        ax[0].imshow(img, cmap="gray", vmin=0, vmax=255)
+        ax[0].set_title("Original")
+        ax[1].imshow(img_approx_clipped, cmap="gray", vmin=0, vmax=255)
+        ax[1].set_title("Top 8 of 64 coeffs per block")
+        for a in ax:
+            a.axis("off")
+        plt.tight_layout()
+        plt.show()
+
+compress_image("bilder/rice.tif")
                     
 
